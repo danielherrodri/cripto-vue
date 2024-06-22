0367d7695bde8930f80de572722621bf361b3fe4
drodriguez <drodriguez@izzi.mx>
2024-06-22 01:45:38 -0600

    Vídeo 153 - Mostrando las criptomonedas en el Formulario

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,16 @@ onMounted(() => {
             <option v-for="moneda in monedas" :value="moneda.codigo">{{ moneda.texto }}</option>
           </select>
         </div>
+        <div class="campo">
+          <label for="cripto">Criptomoneda:</label>
+          <select id="cripto">
+            <option value="">-- Selecciona --</option>
+            <option v-for="criptomoneda in criptoMonedas" :value="criptomoneda.CoinInfo.Name">{{
+              criptomoneda.CoinInfo.FullName }}
+            </option>
+          </select>
+        </div>
+        <input type="submit" value="Cotizar" />
       </form>
     </div>
   </div>
